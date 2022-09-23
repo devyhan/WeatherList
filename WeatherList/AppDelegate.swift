97e1ce44cc40989a611b324339fffdb98a2f30f2
devyhan <devyhan93@gmail.com>
2022-09-23 17:07:55 +0900

    Added Project #1
    - Stooryboard를 제거한 프로젝트를 추가합니다.
    - gitignore를 추가합니다.
    
    close: #1

---
+++ WeatherList/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  WeatherList
+//
+//  Created by 조요한 on 2022/09/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    let viewController = ViewController()
+    self.window?.overrideUserInterfaceStyle = .light
+    self.window?.rootViewController = UINavigationController(rootViewController: viewController)
+    self.window?.makeKeyAndVisible()
+    return true
+  }
+}
+
---
+++ WeatherList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherList
+//
+//  Created by 조요한 on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
